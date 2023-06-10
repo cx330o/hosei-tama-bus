d9bdf32bdaab5d3728412934654582b08e58b77c
cx330o <50682900+cx330o@users.noreply.github.com>
2023-06-10 14:20:00 +0800

    add express boilerplate and basic server setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FastSend API");
+});
+
+const PORT = process.env.PORT || 9003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
