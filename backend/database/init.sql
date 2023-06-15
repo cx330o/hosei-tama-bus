42a5d558a4f5d419e867565f766403cdb2a83618
cx330o <50682900+cx330o@users.noreply.github.com>
2023-06-15 16:45:00 +0800

    write init sql for messages and files tables

---
+++ backend/database/init.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS File;
+DROP TABLE IF EXISTS Message;
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE Message (
+    id INTEGER PRIMARY KEY NOT NULL,
+    creation_time TEXT NOT NULL,
+    expiration_time TEXT NOT NULL,
+    text TEXT
+);
+
+CREATE TABLE File (
+    id INTEGER PRIMARY KEY NOT NULL,
+    message_id INTEGER NOT NULL,
+    file_type TEXT NOT NULL,
+    file_size INTEGER NOT NULL,
+    file_name TEXT NOT NULL,
+    FOREIGN KEY(message_id) REFERENCES Message(id)
+);
