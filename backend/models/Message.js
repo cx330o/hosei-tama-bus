53857269ae7a236285e33ca35c6ead3e33d6d244
cx330o <50682900+cx330o@users.noreply.github.com>
2023-07-05 10:30:00 +0800

    add groupRows helper to nest files under messages

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -13,28 +13,79 @@ function createMessage(text) {
   return result.lastInsertRowid;
 }
 
+// Group flat JOIN rows into nested message objects with their files
+function groupRows(rows) {
+  return rows.reduce((acc, row) => {
+    let message = acc.find((m) => m.message_id === row.message_id);
+    if (!message) {
+      message = {
+        message_id: row.message_id,
+        message_creation_time: row.message_creation_time,
+        message_text: row.message_text,
+        message_files: [],
+      };
+      acc.push(message);
+    }
+
+    if (row.file_id) {
+      message.message_files.push({
+        file_id: row.file_id,
+        file_type: row.file_type,
+        file_size: row.file_size,
+        file_name: row.file_name,
+      });
+    }
+
+    return acc;
+  }, []);
+}
+
 function getMessages({ cursor, limit = 20 } = {}) {
   const currentTime = new Date().toISOString();
 
-  let sql = `SELECT
-    m.id as message_id,
-    m.creation_time as message_creation_time,
-    m.text as message_text
-  FROM Message m
-  WHERE m.expiration_time > ?`;
-  const params = [currentTime];
+  // Fetch message IDs with cursor-based pagination
+  let idSql = `SELECT DISTINCT m.id FROM Message m
+    WHERE m.expiration_time > ?`;
+  const idParams = [currentTime];
 
   if (cursor) {
-    sql += ` AND m.creation_time < ?`;
-    params.push(cursor);
+    idSql += ` AND m.creation_time < ?`;
+    idParams.push(cursor);
   }
 
-  sql += ` ORDER BY m.creation_time DESC LIMIT ?`;
-  params.push(limit + 1);
+  idSql += ` ORDER BY m.creation_time DESC LIMIT ?`;
+  idParams.push(limit + 1);
 
-  const rows = db.prepare(sql).all(...params);
-  const hasMore = rows.length > limit;
-  const messages = rows.slice(0, limit);
+  const idRows = db.prepare(idSql).all(...idParams);
+  const hasMore = idRows.length > limit;
+  const messageIds = idRows.slice(0, limit).map((r) => r.id);
+
+  if (messageIds.length === 0) {
+    return { messages: [], hasMore: false, nextCursor: null };
+  }
+
+  const placeholders = messageIds.map(() => "?").join(",");
+  const dataSql = `SELECT
+    m.id as message_id,
+    m.creation_time as message_creation_time,
+    m.text as message_text,
+    f.id as file_id,
+    f.file_type,
+    f.file_size,
+    f.file_name
+  FROM Message m
+    LEFT JOIN File f ON m.id = f.message_id
+  WHERE m.id IN (${placeholders})
+  ORDER BY m.creation_time DESC`;
+
+  const rows = db.prepare(dataSql).all(...messageIds);
+  const messages = groupRows(rows);
+
+  messages.sort(
+    (a, b) =>
+      Date.parse(b.message_creation_time) -
+      Date.parse(a.message_creation_time)
+  );
 
   const lastMessage = messages[messages.length - 1];
   const nextCursor = hasMore ? lastMessage.message_creation_time : null;
@@ -46,19 +97,28 @@ function getMessage(messageId) {
   const sql = `SELECT
     m.id as message_id,
     m.creation_time as message_creation_time,
-    m.text as message_text
+    m.text as message_text,
+    f.id as file_id,
+    f.file_type,
+    f.file_size,
+    f.file_name
   FROM Message m
+    LEFT JOIN File f ON m.id = f.message_id
   WHERE m.id = ?`;
 
-  const row = db.prepare(sql).get(messageId);
-  if (!row) {
+  const rows = db.prepare(sql).all(messageId);
+
+  if (rows.length === 0) {
     return {
       message_id: null,
       message_creation_time: null,
       message_text: null,
+      message_files: [],
     };
   }
-  return row;
+
+  const grouped = groupRows(rows);
+  return grouped[0];
 }
 
 module.exports = {
