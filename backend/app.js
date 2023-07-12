b0167595aee229c3f8087f215ccf6b233a9ebc84
cx330o <50682900+cx330o@users.noreply.github.com>
2023-07-12 20:15:00 +0800

    add express-async-errors and ensure messages sorted desc

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("express-async-errors");
 const cors = require("cors");
 const messageRouter = require("./routers/messages");
 const middleware = require("./utils/middleware");
