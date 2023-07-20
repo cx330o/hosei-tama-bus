c3cb9da31cac8fd6fa5735c389d82f408c6f3dfe
cx330o <50682900+cx330o@users.noreply.github.com>
2023-07-20 16:45:00 +0800

    add files router for serving uploaded files

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 require("express-async-errors");
 const cors = require("cors");
 const messageRouter = require("./routers/messages");
+const fileRouter = require("./routers/files");
 const middleware = require("./utils/middleware");
 
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/messages", messageRouter);
+app.use("/api/files", fileRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
