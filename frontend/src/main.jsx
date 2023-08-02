bc08847a8a882101ab800391be767d1d56fd3ccf
cx330o <50682900+cx330o@users.noreply.github.com>
2023-08-02 09:45:00 +0800

    import quill bubble theme and primeicons globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,13 @@ import App from "./App.jsx";
 import { PrimeReactProvider } from "primereact/api";
 
 import "./index.css";
+// 鍏ㄥ眬鏍峰紡
+import "quill/dist/quill.bubble.css";
+import "primeicons/primeicons.css";
+// 涓婚鏍峰紡
+// import "primereact/resources/themes/lara-dark-indigo/theme.css";
+// import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PrimeReactProvider>
