a7c0b8f75a8ccee6424a3aa5d6273e7e53bff66f
cx330o <50682900+cx330o@users.noreply.github.com>
2023-08-03 15:20:00 +0800

    create App component with dark background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import "./App.css";
+
 const App = () => {
   return (
-    <div>
-      <h1>FastSend</h1>
-      <p>Coming soon...</p>
+    <div className="flex justify-center min-h-screen bg-[#0e0e0e]">
+      <div className="text-white p-4">
+        <h1>FastSend</h1>
+        <p>Messages will go here</p>
+      </div>
     </div>
   );
 };
