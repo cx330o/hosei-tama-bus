26151ea6fd4a814cd498c6ac6f7ec05336efc778
cx330o <50682900+cx330o@users.noreply.github.com>
2023-08-15 11:30:00 +0800

    add FormattedDateTime with relative time display

---
+++ frontend/src/components/FormattedDateTime.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const FormattedDateTime = ({ dateTime }) => {
+  const now = new Date();
+  const date = new Date(dateTime);
+  const diffMs = now - date;
+  const diffMins = Math.floor(diffMs / 60000);
+  const diffHours = Math.floor(diffMs / 3600000);
+
+  let relativeTime;
+  if (diffMins < 1) {
+    relativeTime = "just now";
+  } else if (diffMins < 60) {
+    relativeTime = `${diffMins}m ago`;
+  } else if (diffHours < 24) {
+    relativeTime = `${diffHours}h ago`;
+  } else {
+    relativeTime = new Intl.DateTimeFormat("en", {
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }).format(date);
+  }
+
+  const fullDateTime = new Intl.DateTimeFormat("zh-CN", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  }).format(date);
+
+  return (
+    <time
+      dateTime={dateTime}
+      title={fullDateTime}
+      className="text-xs text-gray-500 font-mono"
+    >
+      {relativeTime}
+    </time>
+  );
+};
+
+export default FormattedDateTime;
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,7 +1,11 @@
+import FormattedDateTime from "./FormattedDateTime";
+
 const Message = ({ message }) => {
   return (
     <div className="p-4 mx-2 mb-3 bg-[#1a1a1a] rounded-xl border border-gray-800/40">
-      <p className="text-xs text-gray-500 mb-2">{message.message_creation_time}</p>
+      <div className="mb-2">
+        <FormattedDateTime dateTime={message.message_creation_time} />
+      </div>
       <p>{message.message_text}</p>
     </div>
   );
