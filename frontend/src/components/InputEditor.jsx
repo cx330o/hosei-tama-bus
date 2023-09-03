92ee0a0369e8cc9d792323e9de342d068081e720
cx330o <50682900+cx330o@users.noreply.github.com>
2023-09-03 11:00:00 +0800

    add toolbar with bold/italic/list buttons to editor

--- frontend/src/components/InputEditor.jsx
+++ frontend/src/components/InputEditor.jsx
@@ -1,6 +1,15 @@
 import { Editor } from "primereact/editor";
 
 const InputEditor = ({ text, setText }) => {
+  const header = (
+    <span className="ql-formats">
+      <button className="ql-bold"></button>
+      <button className="ql-italic"></button>
+      <button className="ql-list" value="ordered"></button>
+      <button className="ql-list" value="bullet"></button>
+    </span>
+  );
+
   return (
     <div className="card">
       <Editor
@@ -9,6 +18,7 @@ const InputEditor = ({ text, setText }) => {
         placeholder="Input text here..."
         onTextChange={(e) => setText(e.htmlValue || "")}
         style={{ minHeight: "150px" }}
+        headerTemplate={header}
       />
     </div>
   );
