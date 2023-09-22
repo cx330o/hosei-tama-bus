1691c94bd263ab1f23b17dcd6a016f62345a61e3
cx330o <50682900+cx330o@users.noreply.github.com>
2023-09-22 11:30:00 +0800

    add video and audio icons in FileItem

--- frontend/src/components/FileItem.jsx
+++ frontend/src/components/FileItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FileText, Film, Music } from "lucide-react";
 
 function getFileType(file) {
   const type = file.type || file.file_type || "";
@@ -15,32 +16,61 @@ function getFileUrl(file, fileUrlBase) {
   return null;
 }
 
+function formatFileSize(bytes) {
+  if (!bytes && bytes !== 0) return null;
+  if (bytes < 1024) return `${bytes} B`;
+  if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(1)} KB`;
+  return `${(bytes / (1024 * 1024)).toFixed(1)} MB`;
+}
+
 const FileItem = React.memo(({ file, fileUrlBase = "" }) => {
   const fileType = getFileType(file);
   const fileUrl = getFileUrl(file, fileUrlBase);
   const fileName = file.file_original_name || file.name;
+  const fileSize = file.file_size || file.size;
 
   const renderPreview = () => {
-    if (fileType === "image") {
-      return (
-        <img
-          src={fileUrl}
-          alt={fileName}
-          className="w-12 h-12 rounded-lg object-cover object-center"
-        />
-      );
+    switch (fileType) {
+      case "image":
+        return (
+          <img
+            src={fileUrl}
+            alt={fileName}
+            className="w-12 h-12 rounded-lg object-cover object-center ring-1 ring-gray-700"
+          />
+        );
+      case "video":
+        return (
+          <div className="w-12 h-12 rounded-lg bg-gray-800 ring-1 ring-gray-700 flex items-center justify-center">
+            <Film size={20} className="text-gray-400" />
+          </div>
+        );
+      case "audio":
+        return (
+          <div className="w-12 h-12 rounded-lg bg-gray-800 ring-1 ring-gray-700 flex items-center justify-center">
+            <Music size={20} className="text-gray-400" />
+          </div>
+        );
+      default:
+        return (
+          <div className="w-12 h-12 rounded-lg bg-gray-800 ring-1 ring-gray-700 flex items-center justify-center">
+            <FileText size={20} className="text-gray-400" />
+          </div>
+        );
     }
-    return (
-      <div className="w-12 h-12 rounded-lg bg-gray-800 flex items-center justify-center text-gray-400 text-xs">
-        FILE
-      </div>
-    );
   };
 
   return (
     <div className="flex items-center gap-3 p-2 rounded-lg hover:bg-white/5 transition-colors">
       <div className="flex-none">{renderPreview()}</div>
-      <span className="truncate text-sm text-gray-300">{fileName}</span>
+      <div className="flex flex-col flex-auto overflow-hidden min-w-0">
+        <span className="truncate text-sm text-gray-300">{fileName}</span>
+        {fileSize && (
+          <span className="text-xs text-gray-500 mt-0.5">
+            {formatFileSize(fileSize)}
+          </span>
+        )}
+      </div>
     </div>
   );
 });
