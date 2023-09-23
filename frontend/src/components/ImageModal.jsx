726f520641831fbbd84c7f1c4131312dac4b5eae
cx330o <50682900+cx330o@users.noreply.github.com>
2023-09-23 15:00:00 +0800

    add ImageModal for fullscreen image view

---
+++ frontend/src/components/ImageModal.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ImageModal = ({ isOpen, imageUrl, alt, onClick }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 bg-black/70 backdrop-blur-sm flex items-center justify-center z-50 cursor-pointer"
+      onClick={onClick}
+    >
+      <img
+        src={imageUrl}
+        alt={alt}
+        className="max-w-[90vw] max-h-[90vh] rounded-lg shadow-2xl"
+      />
+    </div>
+  );
+};
+
+export default ImageModal;
