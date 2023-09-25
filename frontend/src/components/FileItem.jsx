ff8980782a1e0ac29183798ff0ea902c05961495
cx330o <50682900+cx330o@users.noreply.github.com>
2023-09-25 19:00:00 +0800

    add image click to open modal + video modal

--- frontend/src/components/FileItem.jsx
+++ frontend/src/components/FileItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ImageModal from "./ImageModal";
 import { FileText, Film, Music } from "lucide-react";
 
 function getFileType(file) {
@@ -29,20 +30,52 @@ const FileItem = React.memo(({ file, fileUrlBase = "" }) => {
   const fileName = file.file_original_name || file.name;
   const fileSize = file.file_size || file.size;
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+
   const renderPreview = () => {
     switch (fileType) {
       case "image":
         return (
-          <img
-            src={fileUrl}
-            alt={fileName}
-            className="w-12 h-12 rounded-lg object-cover object-center ring-1 ring-gray-700"
-          />
+          <>
+            <img
+              src={fileUrl}
+              alt={fileName}
+              onClick={toggleModal}
+              className="w-12 h-12 rounded-lg object-cover object-center cursor-pointer ring-1 ring-gray-700 hover:ring-indigo-500/50 transition-all"
+            />
+            <ImageModal
+              isOpen={isModalOpen}
+              imageUrl={fileUrl}
+              alt={fileName}
+              onClick={toggleModal}
+            />
+          </>
         );
       case "video":
         return (
-          <div className="w-12 h-12 rounded-lg bg-gray-800 ring-1 ring-gray-700 flex items-center justify-center">
+          <div
+            onClick={toggleModal}
+            className="relative w-12 h-12 rounded-lg bg-gray-800 ring-1 ring-gray-700 hover:ring-indigo-500/50 cursor-pointer flex items-center justify-center transition-all overflow-hidden"
+          >
             <Film size={20} className="text-gray-400" />
+            {isModalOpen && (
+              <div
+                className="fixed inset-0 bg-black/80 backdrop-blur-sm flex items-center justify-center z-50"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleModal();
+                }}
+              >
+                <video
+                  src={fileUrl}
+                  controls
+                  autoPlay
+                  className="max-w-[90vw] max-h-[80vh] rounded-lg"
+                  onClick={(e) => e.stopPropagation()}
+                />
+              </div>
+            )}
           </div>
         );
       case "audio":
@@ -65,11 +98,11 @@ const FileItem = React.memo(({ file, fileUrlBase = "" }) => {
       <div className="flex-none">{renderPreview()}</div>
       <div className="flex flex-col flex-auto overflow-hidden min-w-0">
         <span className="truncate text-sm text-gray-300">{fileName}</span>
-        {fileSize && (
-          <span className="text-xs text-gray-500 mt-0.5">
-            {formatFileSize(fileSize)}
-          </span>
-        )}
+        <span className="text-xs text-gray-500 mt-0.5">
+          {[formatFileSize(fileSize), fileType === "video" ? "Click to play" : null]
+            .filter(Boolean)
+            .join(" 路 ")}
+        </span>
       </div>
     </div>
   );
