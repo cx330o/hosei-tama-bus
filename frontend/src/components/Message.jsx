b38a0e189c0c2fb6959d71a003ecf6d807df3a92
cx330o <50682900+cx330o@users.noreply.github.com>
2023-10-05 11:30:00 +0800

    add message card hover effect

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -4,7 +4,7 @@ import MessageText from "./MessageText";
 
 const Message = ({ message }) => {
   return (
-    <div className="p-4 mx-2 mb-3 bg-[#1a1a1a] rounded-xl border border-gray-800/40">
+    <div className="message-card p-4 mx-2 mb-3 bg-[#1a1a1a] rounded-xl border border-gray-800/40 hover:border-gray-700/60">
       <div className="mb-2">
         <FormattedDateTime dateTime={message.message_creation_time} />
       </div>
