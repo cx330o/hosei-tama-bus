eaf0d321389bfdd2fb53a7c88333cd702b551d1f
cx330o <50682900+cx330o@users.noreply.github.com>
2023-10-30 11:30:00 +0800

    add DELETE route for messages

--- backend/routers/messages.js
+++ backend/routers/messages.js
@@ -6,15 +6,9 @@ const fileService = require("../models/File");
 router.get("/", (req, res) => {
   try {
     const { cursor, limit } = req.query;
-    const result = messageService.getMessages({
-      cursor,
-      limit: limit ? parseInt(limit, 10) : 20,
-    });
+    const result = messageService.getMessages({ cursor, limit: limit ? parseInt(limit, 10) : 20 });
     res.status(200).json({ data: result });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
+  } catch (error) { console.error(error.message); res.status(500).send("Internal Server Error"); }
 });
 
 router.get("/:messageId", (req, res) => {
@@ -22,10 +16,7 @@ router.get("/:messageId", (req, res) => {
   try {
     const message = messageService.getMessage(messageId);
     res.status(200).json({ data: message });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
+  } catch (error) { console.error(error.message); res.status(500).send("Internal Server Error"); }
 });
 
 router.post("/", upload.array("files"), (req, res) => {
@@ -33,16 +24,18 @@ router.post("/", upload.array("files"), (req, res) => {
   const files = req.files;
   try {
     const messageId = messageService.createMessage(text);
-    if (files && files.length > 0) {
-      files.forEach((file) => fileService.createFile(messageId, file));
-    }
-
+    if (files && files.length > 0) files.forEach((file) => fileService.createFile(messageId, file));
     const message = messageService.getMessage(messageId);
     res.status(200).json({ data: message });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-  }
+  } catch (error) { console.error(error.message); res.status(500).send("Internal Server Error"); }
+});
+
+router.delete("/:id", (req, res) => {
+  const messageId = req.params.id;
+  try {
+    messageService.deleteMessage(messageId);
+    res.status(200).json({ message: "Message deleted successfully" });
+  } catch (error) { console.error(error.message); res.status(500).send("Internal Server Error"); }
 });
 
 module.exports = router;
