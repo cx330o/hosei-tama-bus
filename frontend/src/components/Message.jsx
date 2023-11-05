768ee5c5958eb6212217e199ae229aacc6c04770
cx330o <50682900+cx330o@users.noreply.github.com>
2023-11-05 19:30:00 +0800

    add MessageOptionsDropdown with delete action

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,12 +1,28 @@
 import FormattedDateTime from "./FormattedDateTime";
+import MessageOptionsDropdown from "./MessageOptionsDropdown";
 import MessageFiles from "./MessageFiles";
 import MessageText from "./MessageText";
+import messageService from "../services/message";
+import toast, { Toaster } from "react-hot-toast";
 
-const Message = ({ message }) => {
+const Message = ({ message, onDeleteMessage }) => {
+  const handleDelete = async () => {
+    try {
+      await messageService.deleteMessage(message.message_id);
+      toast.success("Message deleted successfully");
+      onDeleteMessage(message.message_id);
+    } catch (error) {
+      console.error("Error deleting message:", error);
+      toast.error("Failed to delete message");
+    }
+  };
+
   return (
     <div className="message-card p-4 mx-2 mb-3 bg-[#1a1a1a] rounded-xl border border-gray-800/40 hover:border-gray-700/60">
-      <div className="mb-2">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex justify-between items-center mb-2">
         <FormattedDateTime dateTime={message.message_creation_time} />
+        <MessageOptionsDropdown onDelete={handleDelete} />
       </div>
       <MessageText text={message.message_text} />
       <MessageFiles files={message.message_files} />
