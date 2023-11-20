b49fc01b440525a72c96e6ed33734eac92771511
cx330o <50682900+cx330o@users.noreply.github.com>
2023-11-20 14:00:00 +0800

    add useWebSocket hook with auto reconnect

---
+++ frontend/src/hooks/useWebSocket.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+
+const url = import.meta.env.VITE_APP_WEBSOCKET_URL;
+const MAX_RECONNECT_DELAY = 30000;
+const INITIAL_RECONNECT_DELAY = 1000;
+
+const useWebSocket = (onMessageReceived) => {
+  const wsRef = useRef(null);
+  const reconnectDelay = useRef(INITIAL_RECONNECT_DELAY);
+  const reconnectTimer = useRef(null);
+
+  useEffect(() => {
+    function connect() {
+      const ws = new WebSocket(url);
+      wsRef.current = ws;
+      ws.onopen = () => { console.log("WebSocket connected"); reconnectDelay.current = INITIAL_RECONNECT_DELAY; };
+      ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === "newMessage") onMessageReceived(data.message);
+      };
+      ws.onclose = () => { console.log(`WebSocket disconnected. Reconnecting in ${reconnectDelay.current}ms...`); scheduleReconnect(); };
+      ws.onerror = (error) => { console.error("WebSocket error:", error); ws.close(); };
+    }
+    function scheduleReconnect() {
+      reconnectTimer.current = setTimeout(() => {
+        reconnectDelay.current = Math.min(reconnectDelay.current * 2, MAX_RECONNECT_DELAY);
+        connect();
+      }, reconnectDelay.current);
+    }
+    connect();
+    return () => {
+      clearTimeout(reconnectTimer.current);
+      if (wsRef.current) { wsRef.current.onclose = null; wsRef.current.close(); }
+    };
+  }, [onMessageReceived]);
+};
+
+export default useWebSocket;
