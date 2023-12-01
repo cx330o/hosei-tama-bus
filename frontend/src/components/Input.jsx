da219b0a58da67be372fd32ba3fd300c9b21e35a
cx330o <50682900+cx330o@users.noreply.github.com>
2023-12-01 15:00:00 +0800

    add drag and drop file upload to Input

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
-
 import InputEditor from "./InputEditor";
 import InputFileArea from "./InputFileArea";
 import InputToolbar from "./InputToolbar";
@@ -9,68 +8,47 @@ import messageService from "../services/message";
 const Input = () => {
   const [text, setText] = useState("");
   const [files, setFiles] = useState([]);
+  const [dragging, setDragging] = useState(false);
 
-  const handleFilesSelect = (event) => {
-    setFiles((prev) => [...prev, ...Array.from(event.target.files)]);
-  };
-
-  const handleRemoveFile = (fileToRemove) => {
-    setFiles(files.filter((file) => file !== fileToRemove));
-  };
-
+  const handleFilesSelect = (event) => { setFiles((prev) => [...prev, ...Array.from(event.target.files)]); };
+  const handleRemoveFile = (fileToRemove) => { setFiles(files.filter((file) => file !== fileToRemove)); };
   const handlePaste = (event) => {
     const items = event.clipboardData.items;
     for (const item of items) {
-      if (item.type.startsWith("image/")) {
-        const file = item.getAsFile();
-        setFiles((prev) => [...prev, file]);
-        event.preventDefault();
-      }
+      if (item.type.startsWith("image/")) { const file = item.getAsFile(); setFiles((prev) => [...prev, file]); event.preventDefault(); }
     }
+  };
+  const handleDragOver = (e) => { e.preventDefault(); setDragging(true); };
+  const handleDragLeave = (e) => { e.preventDefault(); setDragging(false); };
+  const handleDrop = (e) => {
+    e.preventDefault(); setDragging(false);
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    if (droppedFiles.length > 0) setFiles((prev) => [...prev, ...droppedFiles]);
   };
 
   const handleSend = async () => {
     const hasText = text.replace(/<[^>]*>/g, "").trim().length > 0;
     const hasFiles = files.length > 0;
-
-    if (!hasText && !hasFiles) {
-      toast.error("Please add some text or files before sending");
-      return;
-    }
-
+    if (!hasText && !hasFiles) { toast.error("Please add some text or files before sending"); return; }
     try {
       const formData = new FormData();
       formData.append("text", hasText ? text : "");
-      files.forEach((file) => {
-        formData.append("files", file);
-      });
+      files.forEach((file) => { formData.append("files", file); });
       const newMessage = await messageService.create(formData);
-      if (newMessage) {
-        setText("");
-        setFiles([]);
-      }
-    } catch {
-      toast.error("Failed to send message");
-    }
+      if (newMessage) { setText(""); setFiles([]); }
+    } catch { toast.error("Failed to send message"); }
   };
 
   return (
-    <div
-      className="mx-2 mb-4 bg-[#1a1a1a] rounded-xl border transition-colors duration-200 overflow-hidden border-gray-800/40 hover:border-indigo-500/40 focus-within:border-indigo-500/60"
-    >
+    <div onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDrop}
+      className={`mx-2 mb-4 bg-[#1a1a1a] rounded-xl border transition-colors duration-200 overflow-hidden ${
+        dragging ? "border-indigo-500 bg-indigo-500/5" : "border-gray-800/40 hover:border-indigo-500/40 focus-within:border-indigo-500/60"
+      }`}>
       <Toaster position="top-center" reverseOrder={false} />
-      <div className="p-4">
-        <InputEditor
-          text={text}
-          setText={setText}
-          onPaste={handlePaste}
-          onSend={handleSend}
-        />
-      </div>
+      {dragging && (<div className="px-4 py-3 text-center text-sm text-indigo-400 bg-indigo-500/10 border-b border-indigo-500/20">Drop files here to upload</div>)}
+      <div className="p-4"><InputEditor text={text} setText={setText} onPaste={handlePaste} onSend={handleSend} /></div>
       <InputFileArea files={files} onRemoveFile={handleRemoveFile} />
-      <div className="border-t border-gray-800/30">
-        <InputToolbar onFilesSelected={handleFilesSelect} onSend={handleSend} />
-      </div>
+      <div className="border-t border-gray-800/30"><InputToolbar onFilesSelected={handleFilesSelect} onSend={handleSend} /></div>
     </div>
   );
 };
